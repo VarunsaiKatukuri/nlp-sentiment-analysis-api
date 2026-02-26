658aba65d386c226951891759789de830f2c621c
VarunsaiKatukuri <varunsaikatukuri@gmail.com>
2026-02-26 19:05:45 +0530

    Added Naive Bayes and SVM models with multi model UI support

--- api/app.py
+++ api/app.py
@@ -11,15 +11,25 @@ from fastapi import Response, Request, Form
 from src.data_preprocessing import clean_text
 from src.feature_engineering import transform_tfidf
 
-# loading the saved model and vectorizer
-model_path = os.path.join(BASE_DIR, "models", "logistic_regression.pkl")
-vectorizer_path = os.path.join(BASE_DIR, "models","tfidf_vectorizer.pkl")
-model = joblib.load(model_path)
-vectorizer = joblib.load(vectorizer_path)
+# loading the saved models and vectorizer
+logistic_model = joblib.load(os.path.join(BASE_DIR, "models", "logistic_regression.pkl"))
+naive_bayes_model = joblib.load(os.path.join(BASE_DIR,"models", "naive_bayes.pkl"))
+svm_model = joblib.load(os.path.join(BASE_DIR, "models", "svm.pkl"))
+vectorizer = joblib.load(os.path.join(BASE_DIR, "models","tfidf_vectorizer.pkl"))
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+# creating model selection function
+def get_model(model_name: str):
+    if model_name == "logistic":
+        return logistic_model
+    elif model_name == "naive_bayes":
+        return naive_bayes_model
+    elif model_name == "svm":
+        return svm_model
+    else:
+        return logistic_model   #default model
 
 class ReviewRequest(BaseModel):
     review: str
@@ -28,11 +38,24 @@ class ReviewRequest(BaseModel):
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 @app.post("/predict_form", response_class=HTMLResponse)
-def predict_form(request: Request, review: str = Form(...)):
+def predict_form(
+    request: Request,
+    review: str = Form(...),
+    model_choice: str = Form(...)
+):
+
+    model = get_model(model_choice)
+
     cleaned = clean_text(review)
     vectorized = transform_tfidf(vectorizer, [cleaned])
     prediction = model.predict(vectorized)[0]
-    probability = model.predict_proba(vectorized)[0].max()
+
+    # Handle probability safely
+    if hasattr(model, "predict_proba"):
+        probability = model.predict_proba(vectorized)[0].max()
+        probability = round(float(probability), 3)
+    else:
+        probability = "Not Available"
 
     sentiment = "positive" if prediction == 1 else "negative"
 
@@ -40,5 +63,6 @@ def predict_form(request: Request, review: str = Form(...)):
         "request": request,
         "review": review,
         "prediction": sentiment,
-        "confidence": round(float(probability), 3)
+        "confidence": probability,
+        "selected_model": model_choice
     })
